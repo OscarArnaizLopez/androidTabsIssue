e3552a521835dec0a43c4ca17e6da724e10f0262
Oscar Arnaiz <oscar@appointedd.com>
2016-10-03 12:12:59 +0100

    Fixed. ClearFocus is called through loaded event

--- app/app.component.js
+++ app/app.component.js
@@ -1,8 +1,18 @@
 "use strict";
 var core_1 = require("@angular/core");
+var app = require("application");
 var AppComponent = (function () {
     function AppComponent() {
     }
+    AppComponent.prototype.onSearchBarLoaded = function () {
+        if (app.android) {
+            this.searchbar.nativeElement.android.clearFocus();
+        }
+    };
+    __decorate([
+        core_1.ViewChild("searchbar"), 
+        __metadata('design:type', core_1.ElementRef)
+    ], AppComponent.prototype, "searchbar", void 0);
     AppComponent = __decorate([
         core_1.Component({
             selector: "my-app",
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,21 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, ViewChild} from "@angular/core";
+import {SearchBar} from "ui/search-bar";
+import app = require("application");
 
 @Component({
     selector: "my-app",
     templateUrl: "app.component.html",
 })
 export class AppComponent {
+
+    @ViewChild("searchbar") searchbar: ElementRef;
     
+
+    constructor(){}
+    
+    onSearchBarLoaded(){
+        if (app.android) {
+            this.searchbar.nativeElement.android.clearFocus();
+        }
+    }
 }
